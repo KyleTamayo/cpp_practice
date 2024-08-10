fd8426a7021f3a22d91b5858e83f609c31bccb7f
kyletamayo <kyle_tamayo@startmail.com>
2024-08-10 17:49:57 -0400

    Still bugged, working out the pointer issue

--- main.cpp
+++ main.cpp
@@ -1,6 +1,97 @@
 #include <iostream>
+#include <utility>
 
+class Goblin;
+class Player;
+
+class Weapon {
+public:
+    std::string _name{"Iron Sword"};
+};
+
+class Shield {
+public:
+    std::string _name{"Omega Shield"};
+};
+
+class Player {
+public:
+    Weapon* p_weapon{nullptr};
+    Shield* p_shield{nullptr};
+
+    Player() {
+        _Name = "John";
+    };
+    explicit Player(std::string Name): _Name(std::move(Name)){}
+
+    std::string GetName(){ return _Name; }
+    void SetEnemy(Goblin* Enemy) {
+        mEnemy = Enemy;
+    }
+
+    void LogEnemy() {
+        if(mEnemy)
+            std::cout << "\nEnemy: " << mEnemy->GetName();
+        else
+            std::cout << "\nOops! No enemy!";
+    }
+
+private:
+    std::string _Name;
+    Goblin* mEnemy{nullptr};
+};
+
+class Goblin {
+public:
+    Goblin() = default;
+    explicit Goblin(std::string Name): _Name(std::move(Name)) {}
+
+    std::string GetName(){ return _Name; }
+private:
+    std::string _Name;
+};
+
+void IncrememntReference(int& Number) { // Takes the reference to the integer
+    Number++; // Increments Number
+}
+void IncrementPointer(int* Number) { // Takes a pointer to an integer
+    (*Number)++; // Dereferences pointer before incrementing
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x{10};
+    Player PlayerOne
+    {"Tom"};
+    Weapon Sword;
+    Shield Shield;
+    if(!PlayerOne
+    .p_weapon)
+        std::cout << "Unarmed";
+    PlayerOne
+    .p_weapon = &Sword;
+    if(PlayerOne
+    .p_weapon)
+        std::cout << "\nNOT NOW! " << PlayerOne
+        .p_weapon->_name << std::endl;
+    if(!PlayerOne
+    .p_shield)
+        std::cout << "\nNO SHIELD!!";
+    PlayerOne
+    .p_shield = &Shield;
+    if(PlayerOne
+    .p_shield)
+        std::cout << "\nNow I have one! " << PlayerOne
+        .p_shield->_name << std::endl;
+    std::cout << x << std::endl;
+    IncrememntReference(x);  // Sends integer normally
+    std::cout << x << std::endl;
+    IncrementPointer(&x); // Sends reference to memory address
+    std::cout << x << std::endl;
+
+    std::cout << "Player One is: " << PlayerOne.GetName()
+        << "\nA new enemy has appeared! ";
+    Goblin Gobbo{"Gobbo"};
+    PlayerOne.SetEnemy(&Gobbo);
+    PlayerOne.LogEnemy();
+
     return 0;
 }
