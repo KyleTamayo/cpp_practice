b14384831140defae015fb51509c2069900bec53
kyletamayo <kyle_tamayo@startmail.com>
2024-09-02 15:54:10 -0400

    Playing around

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <vector>
 #include "Player.hpp"
+#include "Test.hpp"
 
-int main(){
+int main() {
     Player player;
+    std::vector<int> TestVectorOne;
+    std::vector TestVectorTwo{1, 2, 3, 4, 5};
 
-    std::cout << player.GetHealth();
+    TestVectorOne = {6, 7, 8, 9, 10};
+
+    std::cout << player.GetHealth() << "\n";
     player.SetHealth(150);
-    std::cout << player.GetHealth();
+    std::cout << TestSpace::Pi << "\n";
+    std::cout << player.GetHealth() << "\n";
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << TestVectorTwo[i] << "\n";
+    }
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << TestVectorOne[i] << "\n";
+    }
 }
---
+++ src/Test.hpp
@@ -0,0 +1,12 @@
+//
+// Created by kyle on 9/1/24.
+//
+
+#ifndef TEST_HPP
+#define TEST_HPP
+
+namespace TestSpace {
+    constexpr float Pi{3.14};
+}
+
+#endif //TEST_HPP
